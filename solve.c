da390beb59cc2d7d5c602824bfbff1967bba269f
Jackson Taylor <jtaylor@e1z2r5p6.42.us.org>
2018-11-13 16:56:32 -0800

    framework (?)

---
+++ solve.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solve.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/07 08:05:31 by jtaylor           #+#    #+#             */
+/*   Updated: 2018/11/08 16:01:20 by jtaylor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*	Solve
+**		declares a struct for the tetriminoes
+**		calls func to find the pieces from input file
+**		calls func to \"solve \" the pieces
+**		returns 0 if either func fails , otherwise returns 1;
+*/
+
+int	solve(char const *filename)
+{
+	//declare struct for tetriminoes ;
+
+	// get tetriminoes ;
+
+	// solve squares ;
+	return (1);
+}
